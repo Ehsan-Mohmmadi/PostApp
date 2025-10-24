42f06d2a34df6a7d975a71344e1d67920e39172d
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-10-24 17:51:13 +0330

    Update UserController.php
    Add register function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+    public function register(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/');
+    }
 }
